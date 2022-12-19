ff0ed775227202357f9a9536fe5405b98a290474
Nick <Nickishero@gmail.com>
2022-12-19 19:10:51 +0100

    Refactor

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# hagrotooling
- 
+# Hagrotooling
+
+Met deze tool kunnen materiaaltabellen van een divers aantal vreemdtalige pdf's worden ingeladen.
+
+Per 'vreemdtalige' pdf moet een structuur worden aangegeven. Bijvoorbeeld, de materiaaltabellen in pdf's van *Fabrikant X* hebben beginnen altijd met als eerste kolomnaam: "*Werkstoffnummer*".
+Deze structuur kan worden ingesteld en opgeslagen voor toekomstig gebruik.
+
+Door middel van de ingestelde structuur kan worden afgeleid welke tabellen deze tool moet opslaan. Het handmatig instellen van een bepaalde structuur is nodig, omdat de pdf's namelijk uit meerdere tabellen kunnen bestaan; Waaronder tabellen die helemaal niks te maken hebben met materialen. 
+
+### Werkende Functionaliteiten:
+- User interface met [PyQt5](https://pypi.org/project/PyQt5/)
+- Selecteren van één of meer PDF bestanden
+- Het inlezen van tabellen door middel van [Tabula](https://pypi.org/project/tabula-py/)
+
+
+### Todo's:
+- (PRIO) Het omzetten van de uitgelezen datastream naar een leesbaar formaat zoals JSON
+- (PRIO) Het instellen en uitlezen van een configureerbare structuur per fabrikant voor de pdf's.
+- Het converteren van kolomnamen naar een vooraf bepaalde naam (Zie hierboven)
+
+### Verbeterpunten:
+- Het lezen van de pdf's op een andere Thread runnen zodat de applicatie niet freezed bij grote bestanden (bijvoorbeeld, een pdf van 300 pagina's).
+- Het tonen van een laadbalk.
--- main.py
+++ main.py
@@ -1,18 +1,54 @@
+import sys
 import tabula
-import csv
 
-pdf_path = './testdata/fraeskatalog_milling_catalogue_1.pdf'
-alternative = './testdata/schema.pdf'
-output_path = './results/output.csv'
+from PyQt5.QtCore import QThread, QTimer
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QFileDialog, QVBoxLayout, QWidget, QProgressBar
 
-dfs = tabula.read_pdf(pdf_path, stream=True, pages=6)
-print(dfs[0])
+# Create the main window
 
-with open('./results/output2.csv', 'w') as f:
+app = QApplication(sys.argv)
+window = QWidget()
+window.setMinimumSize(300, 200)
+window.setWindowTitle("Hagrotooling")
+window.setStyleSheet("QWidget {background-color: #2b2b2b; color: white;} QPushButton {background-color: #3592c4;}")
 
-    writer = csv.writer(f)
+# Create layout
+layout = QVBoxLayout()
 
-    for value in dfs[0].values:
-        writer.writerow(value)
+# Create a label and set its text
+label = QLabel("U heeft nog geen PDF bedstanden geselecteerd.")
+layout.addWidget(label)
 
-tabula.convert_into(alternative, output_path, output_format='csv', pages='all')
+# Create a button to open the file selection dialog
+button = QPushButton("Selecteer PDF bestanden")
+layout.addWidget(button)
+
+
+# Create a function to handle the button click event
+def on_button_clicked():
+    # Open the file selection dialog
+    file_names, _ = QFileDialog.getOpenFileNames(window, "Selecteer PDF bestanden met materiaaltabellen", "",
+                                                 "PDF Files (*.pdf)")
+
+    # Update the label text
+    label.setText("Geselecteerde PDF bestanden:\n" + "\n".join(file_names))
+    load_pdf(file_names)
+
+
+def load_pdf(file_names):
+    # read the PDF file using tabula
+    tables = tabula.read_pdf(file_names[0], stream=True, pages="all", multiple_tables=True)
+
+    for table in tables:
+        print(table)
+
+
+# Connect the button click event to the function
+button.clicked.connect(on_button_clicked)
+
+# Show the main window
+window.setLayout(layout)
+window.show()
+
+# Run the main loop
+sys.exit(app.exec_())
